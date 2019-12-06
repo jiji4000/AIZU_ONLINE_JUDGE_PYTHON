8069d6277ae6b18a9ae2882178b901255b8b7831
jiji4000 <jiji4000alpha@gmail.com>
2019-12-06 23:14:52 +0900

    update 0022.py

--- Volume0/0022.py
+++ Volume0/0022.py
@@ -6,28 +6,32 @@ while 1:
     if(n == 0):break
 
     sum = 0
+    arr = []
     isPlus = True
     maxValue = -100000
     for i in range (n):
         try:
-            num = int(input())
-            maxValue = max(maxValue,num)
+            arr.append(int(input()))  
         except:break
         
         # 最初から-が続いていた場合は、意味がないので無視
-        if num < 0 and sum > 0:
-            # plusが連続して続いていた場合
-            if(isPlus):
-                # それまでの合計値を保存する
-                maxValue = max(maxValue,sum)
-                isPlus = False
-            sum += num
-        # plusの場合
-        elif(num > 0):
-            if(sum < 0) :
-                sum = 0
-            sum += num
+        if arr[i] < 0:
+            if sum > 0:
+                # plusが連続して続いていた場合
+                if(isPlus):
+                    # それまでの合計値を保存する
+                    maxValue = max(maxValue,sum)
+                    isPlus = False
+                sum += arr[i]
+                if sum < 0 :
+                    sum = 0
+        else :
+            sum += arr[i]
             isPlus = True
     if sum != 0 :
         maxValue = max(maxValue,sum)
+    # 全てminusなので、入力値から最小のものを探す    
+    else :
+        for i in arr:
+            maxValue = max(maxValue,i)
     print(maxValue)
