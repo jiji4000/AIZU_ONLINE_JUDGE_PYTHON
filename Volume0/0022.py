9585faf2ffceed6d5a71ca88b064fca089ec99c4
jiji4000 <jiji4000alpha@gmail.com>
2019-12-06 22:55:32 +0900

    0022.py 改良

--- Volume0/0022.py
+++ Volume0/0022.py
@@ -7,7 +7,6 @@ while 1:
 
     sum = 0
     isPlus = True
-    isPush = False
     maxValue = -100000
     for i in range (n):
         try:
@@ -16,27 +15,19 @@ while 1:
         except:break
         
         # 最初から-が続いていた場合は、意味がないので無視
-        if num < 0 and isPush:
+        if num < 0 and sum > 0:
             # plusが連続して続いていた場合
             if(isPlus):
                 # それまでの合計値を保存する
                 maxValue = max(maxValue,sum)
                 isPlus = False
-                sum += num
-            # minusが続いていた場合
-            else:
-                sum += num
+            sum += num
         # plusの場合
         elif(num > 0):
-            # plusが続いていた場合
-            if(isPlus):
-                isPush = True
-                sum += num
-            else:
-                if (sum < 0):
-                    sum = 0
-                isPlus = True
-                sum += num
-    if isPush :
+            if(sum < 0) :
+                sum = 0
+            sum += num
+            isPlus = True
+    if sum != 0 :
         maxValue = max(maxValue,sum)
     print(maxValue)
