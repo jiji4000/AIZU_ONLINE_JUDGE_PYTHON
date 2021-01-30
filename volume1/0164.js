86270b7e61c1f200812bcfc88914e0b748942d5b
jiji4000 <jiji4000alpha@gmail.com>
2021-01-30 22:07:21 +0900

    add 0164.js

---
+++ volume1/0164.js
@@ -0,0 +1,26 @@
+let input = require('fs').readFileSync('/dev/stdin', 'utf8')
+let Arr = (input.trim()).split("\n")
+
+for(var i = 0;i < Arr.length - 1;i += 2){
+    let n = Arr[i]
+    let arr = Arr[i + 1].split(" ").map(Number)
+    let turn = false
+    let index = 0
+    let ohajiki = 32
+    while(ohajiki > 0){
+        if(turn){
+            ohajiki -= arr[index]
+            if(ohajiki < 0){
+                ohajiki = 0
+            }
+            if(++index >= n){
+                index = 0
+            }
+        }
+        else{
+            ohajiki -= (ohajiki - 1) % 5
+        }
+        turn = !turn
+        console.log(ohajiki)
+    }
+}
