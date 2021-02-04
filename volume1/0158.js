326c3aef18cb9585a72330d54fef6129cb78216f
jiji4000 <jiji4000alpha@gmail.com>
2021-02-05 01:11:47 +0900

    add 0158.js

---
+++ volume1/0158.js
@@ -0,0 +1,18 @@
+let input = require('fs').readFileSync('/dev/stdin', 'utf8')
+let Arr = (input.trim()).split("\n")
+
+for(var i = 0;i < Arr.length - 1;++i){
+    let n = Arr[i]
+    let ans = 0
+    while(n != 1){
+        if(n % 2 == 0){
+            n = parseInt(n / 2)
+        }
+        else{
+            n = (n * 3) + 1
+        }
+        ans++
+    }
+    console.log(ans)
+}
+ 
